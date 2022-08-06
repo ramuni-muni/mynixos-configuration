4136bf228b2312086722c3a27ea1595f5d068cbd
ramuni-muni <100084739+ramuni-muni@users.noreply.github.com>
2022-08-06 23:59:38 +0700

    add desktop manager

--- configuration.nix
+++ configuration.nix
@@ -4,194 +4,284 @@
 
 { config, pkgs, ... }:
 
+let
+	#sway config for DM
+	swayConfig = pkgs.writeText "greetd-sway-config" ''
+		output * bg ${pkgs.pantheon.elementary-wallpapers}/share/backgrounds/Nattu Adnan.jpg fill
+    		# `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
+    		exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s /etc/greetd/gtkgreet.css -c sway; swaymsg exit"
+		exec "${pkgs.waybar}/bin/waybar -c /etc/greetd/waybar"
+    		bindsym Mod4+q exec swaynag \
+      		-t warning \
+      		-m 'What do you want to do?' \
+      		-b 'Poweroff' 'systemctl poweroff' \
+      		-b 'Reboot' 'systemctl reboot'
+  	'';
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  	imports =
+    	[ # Include the results of the hardware scan.
+      		./hardware-configuration.nix
+    	];
 
-  # kernel
-  #boot.kernelPackages = pkgs.linuxPackages_5_18;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+	#greetd DM
+	services.greetd = {
+	    	enable = true;
+	    	settings = {
+	      		default_session = {
+	        		command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+	      		};
+	    	};
+		package = pkgs.greetd.gtkgreet;
+	};
+	
+	#waybar config for DM
+	environment.etc."greetd/waybar".text = ''
+		{
+	    	"layer": "top",
+    		"position": "bottom",
+    		"height": 24,
+    		"spacing": 0,
+    		"margin-top": 0,
+    		"margin-bottom": 0,
+    		"mode": "dock",
+    		"exclusive": true,
+		"ipc": true,
+		
+    		"modules-left":[ "custom/launcher" ],
+    		"modules-right":[ "custom/exit" ],
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sdc"; # or "nodev" for efi only
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.extraEntries = ''
-  	# GRUB 1 example (not GRUB 2 compatible)
-  	#title Windows
-  	 # chainloader (hd0,1)+1
-  
-  	# GRUB 2 example
-  	#menuentry "Windows 7" {
-  	  #chainloader (hd0,4)+1
-  	#}
-  
-  	# GRUB 2 with UEFI example, chainloading another distro
-  	#menuentry "Fedora" {
-  	  #set root=(hd1,1)
-  	  #chainloader /efi/fedora/grubx64.efi
-  	#}
-  '';
-  boot.supportedFilesystems = 
-	[
-  		"btrfs"
+		"custom/launcher":{
+		        "format": "   Reboot ↻",
+		        "on-click": "systemctl reboot"
+		},
+		"custom/exit":{
+			"format": " ⏻ Shutdown    ",
+		        "on-click": "systemctl poweroff"
+		}
+		}
+	 '';
+
+	#available DE
+	environment.etc."greetd/environments".text = ''
+	    	sway
+	    	bash
+	 '';
+
+	#CSS for gtkgreet DM
+	environment.etc."greetd/gtkgreet.css".text = ''
+		window {
+			color: white;
+			background-color: rgba(50, 50, 50, 0);
+		   	/*background-image: url("file:///usr/share/backgrounds/background.jpg");
+	   		background-size: cover;
+	   		background-position: center;*/
+		}
+		box#body {	   
+			color: white;
+			background-color: rgba(50, 50, 50, 0.3);
+			border-radius: 10px;
+		   	padding: 50px;
+		}
+ 	'';
+	
+
+	# allow non free packet
+	nixpkgs.config.allowUnfree = true;
+
+	# waydroid
+	#virtualisation = {
+		#waydroid. enable = true;
+    		#lxd.enable = true;
+  	#};
+        
+	#virtual box
+	#virtualisation.virtualbox.host.package = pkgs.virtualbox;
+	#virtualisation.virtualbox.guest.enable = true;
+	#virtualisation.virtualbox.host.enable = true;
+	#virtualisation.virtualbox.host.addNetworkInterface = true;
+	#services.boinc.extraEnvPackages = [ pkgs.ocl-icd pkgs.linuxPackages.nvidia_x11 ];
+
+  	# kernel
+  	boot.kernelPackages = pkgs.linuxPackages_latest;
+  	#boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  	#boot.kernelPackages = pkgs.linuxPackages_zen;
+
+	#Filesystem
+	boot.supportedFilesystems = [
+	  	"btrfs"
 		"ntfs"
 		"fat32"
 		"fat16"
 	];
 
-  # zram
-  zramSwap.enable = true;
+  	# Use the GRUB 2 boot loader.
+  	boot.loader.grub.enable = true;
+  	boot.loader.grub.version = 2;
+  	# boot.loader.grub.efiSupport = true;
+  	# boot.loader.grub.efiInstallAsRemovable = true;
+  	# boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  	# Define on which hard drive you want to install Grub.
+  	boot.loader.grub.device = "/dev/sdb"; # or "nodev" for efi only
+  	boot.loader.grub.useOSProber = true;
+  	boot.loader.grub.extraEntries = ''
+		# replace --fs-uuid with your partition uuid
+		# boot menu for SLAX
+   		menuentry "slax" {
+   			search --set=slax --fs-uuid 6b16ff59-11ba-406a-bcf6-1ae032ed24c4
+   			linux ($slax)/slax/boot/vmlinuz vga=normal initrd=($slax)/slax/boot/initrfs.img load_ramdisk=1 prompt_ramdisk=0 rw printk.time=0 consoleblank=0 slax.flags=perch,automount   
+   			initrd ($slax)/slax/boot/initrfs.img
+  		}
+	
+		# boot menu for POSROG
+		#menuentry "Android" {
+		#	echo "Phoenix OS Republic Of Game"
+		#	insmod all_video
+		#	search --set=rog --fs-uuid 6b16ff59-11ba-406a-bcf6-1ae032ed24c4
+		#	linux ($rog)/posrog/kernel root=/dev/ram0 androidboot.selinux=permissive SRC= DATA= 
+		#	initrd ($rog)/posrog/initrd.img
+		#}
+	'';
+  
+  	# zram
+  	zramSwap.enable = true;
 
-  networking.hostName = "RamUniX"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+	#network
+  	networking.hostName = "Nixos"; # Define your hostname.
+  	# Pick only one of the below networking options.
+  	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  	networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "Asia/Jakarta";
+  	# Set your time zone.
+  	time.timeZone = "Asia/Jakarta";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  	# Configure network proxy if necessary
+  	# networking.proxy.default = "http://user:password@proxy:port/";
+  	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
+  	# Select internationalisation properties.
+  	i18n.defaultLocale = "en_US.utf8";
+	
+  	# Enable the X11 windowing system.
+  	services.xserver.enable = false;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  	# desktop manager
+  	services.xserver.displayManager.lightdm.enable = false;
+  	services.xserver.desktopManager.plasma5.enable = false;
+	services.xserver.windowManager.icewm.enable = false;
 
-  # desktop manager
-   services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.displayManager.lightdm.greeters.pantheon.enable = true;
+  	# desktop environtment
+  	programs.sway.enable = true;
+  	programs.sway.extraPackages = with pkgs; [
+		waybar
+    		rofi-wayland  		
+		sway-contrib.grimshot
+		grim
+		slurp		
+  	];
+  	hardware.opengl.enable = true;
+  	programs.sway.extraSessionCommands = ''
+  		# SDL:
+  		export SDL_VIDEODRIVER=wayland
+  		# QT (needs qt5.qtwayland in systemPackages):
+  		export QT_QPA_PLATFORM=wayland
+  		export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+  		# Fix for some Java AWT applications (e.g. Android Studio),
+  		# use this if they aren't displayed properly:
+  		export _JAVA_AWT_WM_NONREPARENTING=1
+  		export XCURSOR_THEME=Numix-Cursor
+		#export XDG_CURRENT_DESKTOP=Unity
+  	'';
+  	programs.sway.wrapperFeatures.gtk = true;
+	programs.waybar.enable = true;
+	programs.qt5ct.enable = true;
 
-  # desktop environtment
-  # services.xserver.desktopManager.pantheon.enable = true;
-  programs.sway.enable = true;
-  programs.sway.extraPackages = with pkgs; [
-  	rofi 
-	waybar
-	sway-contrib.grimshot
-	grim
-	slurp
-	lxqt.qterminal
-  ];
-  hardware.opengl.enable = true;
-  programs.sway.extraSessionCommands = 
-''
-  # SDL:
-  export SDL_VIDEODRIVER=wayland
-  # QT (needs qt5.qtwayland in systemPackages):
-  export QT_QPA_PLATFORM=wayland-egl
-  export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-  # Fix for some Java AWT applications (e.g. Android Studio),
-  # use this if they aren't displayed properly:
-  export _JAVA_AWT_WM_NONREPARENTING=1
-  export XCURSOR_THEME=Numix-Cursor
-'';
- programs.sway.wrapperFeatures.gtk = true;
+  	# Enable CUPS to print documents.
+  	services.printing.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
+  	# Enable sound.
+  	security.rtkit.enable = true;
+  	hardware.pulseaudio.enable = false;
+  	services.pipewire.enable = true;
+  	services.pipewire.pulse.enable = true;
+  	services.pipewire.jack.enable = true;
+  	services.pipewire.alsa.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  	# Enable touchpad support (enabled default in most desktopManager).
+  	services.xserver.libinput.enable = true;
 
-  # Enable sound.
-  security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
-  services.pipewire.enable = true;
-  services.pipewire.pulse.enable = true;
-  services.pipewire.jack.enable = true;
-  services.pipewire.alsa.enable = true;
+  	# Define a user account. Don't forget to set a password with ‘passwd’.
+  	users.users.guest = {
+     		isNormalUser = true;
+     		extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+     		packages = with pkgs; [  ];
+  	};
 
+	# Fonts
+  	fonts.fonts = with pkgs; [
+		font-awesome
+  	];
+  	# List packages installed in system profile. To search, run:
+  	environment.systemPackages = with pkgs; [
+		chromium
+		xdg-utils
+    		wget
+		curl
+    		htop
+    		lxtask
+		neofetch
+		libappindicator		
+		networkmanagerapplet
+		tango-icon-theme
+		pop-gtk-theme
+		glib
+		numix-cursor-theme		
+		gparted
+		baobab
+		lxterminal		
+		xfce.mousepad
+		pavucontrol
+		lxqt.pcmanfm-qt
+		lxqt.lximage-qt
+   		lxde.lxmenu-data
+		xarchiver
+		zip
+		unzip
+		vlc	
+		
+		pantheon.elementary-wallpapers
+	];
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  	# Some programs need SUID wrappers, can be configured further or are
+  	# started in user sessions.
+   	#programs.mtr.enable = true;
+   	#programs.gnupg.agent = {
+    	#enable = true;
+     	#enableSSHSupport = true;
+   	#};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.guest = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     thunderbird
-  #   ];
-  };
+  	# List services that you want to enable:
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-	lxqt.lximage-qt
-	xfce.mousepad
-	pcmanfm-qt
-	neofetch
-	htop
-	lxtask
-	xarchiver
-	zip
-	unzip
-	chromium
-	gparted
-	baobab
-	tango-icon-theme
-	font-awesome
-	pop-gtk-theme
-	pavucontrol
-	networkmanagerapplet
-	glib
-	numix-cursor-theme
-  ];
+  	# Enable the OpenSSH daemon.
+  	# services.openssh.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  	# Open ports in the firewall.
+  	# networking.firewall.allowedTCPPorts = [ ... ];
+  	# networking.firewall.allowedUDPPorts = [ ... ];
+  	# Or disable the firewall altogether.
+  	# networking.firewall.enable = false;
 
-  # List services that you want to enable:
+  	# Copy the NixOS configuration file and link it from the resulting system
+  	# (/run/current-system/configuration.nix). This is useful in case you
+  	# accidentally delete configuration.nix.
+  	# system.copySystemConfiguration = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
-
+  	# This value determines the NixOS release from which the default
+  	# settings for stateful data, like file locations and database versions
+  	# on your system were taken. It‘s perfectly fine and recommended to leave
+  	# this value at the release version of the first install of this system.
+  	# Before changing this value read the documentation for this option
+  	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  	system.stateVersion = "22.05"; # Did you read the comment?
 }
 
